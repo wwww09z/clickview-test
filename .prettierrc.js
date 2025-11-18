9758e5ad55aae0c5e652964701434161ac91d3e9
Allen <allen359857508@gmail.com>
2025-11-18 22:09:29 +1100

    clean up next js template and add basic repo config

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	// plugins: ['prettier-plugin-tailwindcss'],
+	arrowParens: 'avoid',
+	bracketSameLine: true,
+	bracketSpacing: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 2,
+	useTabs: false,
+	semi: false,
+	printWidth: 100,
+};
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  devIndicators: false,
+  allowedDevOrigins: ['127.0.0.1'],
+}
 
-export default nextConfig;
+export default nextConfig
