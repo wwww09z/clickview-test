58ce436a29f54f7d47e815bdbcfc97f05eecebbc
Allen <allen359857508@gmail.com>
2025-11-20 21:50:34 +1100

    target root folder

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   devIndicators: false,
   allowedDevOrigins: ['127.0.0.1'],
+  turbopack: {
+    root: __dirname,
+  },
 }
 
 export default nextConfig
